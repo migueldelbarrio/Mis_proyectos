97a8f7aed82ee7a689b0c1420be1e7e0151b66fb
Miguel del Barrio <miguel@iMac-de-Miguel.local>
2015-07-05 12:44:15 +0200

    session5

--- models/models.js
+++ models/models.js
@@ -34,6 +34,8 @@ exports.User= User;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
+
+  User.create({nombre: 'Miguel', pass:'1234'});
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
